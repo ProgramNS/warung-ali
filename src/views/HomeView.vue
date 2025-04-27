cf45f5234e8441cfc76f9f22c25c6a9bee82cb36
ProgramNS <104614866+ProgramNS@users.noreply.github.com>
2025-04-27 19:54:56 +0700

    fix marker maps

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -29,6 +29,15 @@ const showWhatsAppCard = () => {
   })
 }
 
+// Override default icon
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
+
 onMounted(() => {
   nextTick(() => {
     const map = L.map('map', {
